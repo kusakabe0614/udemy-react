7b13408e5464530d1625f1e30cf6fbef3e4e75bb
kusakabe0614 <ryoma0614.1996@gmail.com>
2021-03-23 17:08:28 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  const greeting = 'Hi'
-  const dom = <div className="App">{greeting}</div>
+// function App() {
+//   const greeting = 'Hi'
+//   const dom = <div className="App">{greeting}</div>
+//   return (
+//     dom
+//   );
+// }
+
+const App = () => {
   return (
-    dom
-  );
+    <div>
+      <Cat></Cat>
+      <Cat></Cat>
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Meow</div>
+}
 export default App;
